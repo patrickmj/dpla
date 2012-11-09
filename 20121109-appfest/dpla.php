3be8879b0d989dfa29f43431709157a766fe0a9a
John Blyberg <john@blyberg.net>
2012-11-09 11:44:41 -0500

    Broadened search skel

--- 20121109-appfest/dpla.php
+++ 20121109-appfest/dpla.php
@@ -12,4 +12,20 @@ class dpla
 	{
 		$this->api_base_url = 'http://api.dp.la/v1/';
 	}
+
+	public function search_item($searchtype, $term) {
+		
+		$searchtype = strtolower($searchtype);
+		$valid_types = array('title', 'description', 'subject', 'dplacontributor', 'creator', 'type', 'publisher', 'format', 'rights', 'contributor', 'spatial', 'ispartof');
+		if (!in_array($searchtype, $valid_types)) {
+			return array('error' => 'Invalid search type');
+		}
+
+		
+	}
+
+	public function item_fetch($item_id) {
+
+	}
+
 }
